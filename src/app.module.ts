854e9ac5404906717e2cbc361d8aa2c61892a0b7
renalex <aleksandr@codetime.am>
2023-10-22 11:52:35 +0400

    add env run and https

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeesService } from './coffees/coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -19,13 +20,14 @@ import { PubSubModule } from './pub-sub/pub-sub.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
       logging: ['query'],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as fs from 'fs';
+import * as https from 'https';
 
 async function bootstrap() {
+  ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` });
+
+  const PORT = process.env.PORT || 4000;
+  const HOST = process.env.HOST || 'localhost';
+
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  let protocol = 'http';
+
+  if (process.env.NODE_ENV === 'prod') {
+    const httpsOptions = {
+      key: fs.readFileSync('path_to_your_private_key.pem'),
+      cert: fs.readFileSync('path_to_your_certificate.pem'),
+    };
+
+    const server = https.createServer(httpsOptions, app.getHttpAdapter().getInstance());
+
+    await app.init();
+    server.listen(PORT);
+
+    protocol = 'https';
+  } else {
+    await app.listen(PORT);
+  }
+
+  console.log(`
+  🚀  GraphQL API server, launched at ${protocol}://${HOST}:${PORT}/graphql`);
 }
 bootstrap();
